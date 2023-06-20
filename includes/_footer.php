8868010821d71cbbf6dd5eaf39dfb2204288e938
edgardoPlata <edgardoplata2001@gmail.com>
2023-06-20 16:30:13 -0600

    seciones

---
+++ includes/_footer.php
@@ -0,0 +1,37 @@
+<footer id= "footer" class="text-center text-lg-start bg-dark text-muted">
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4  border-bottom">
+
+    <div class="me-3 d-none d-lg-block">
+      <span>Siguenos en Facebook:</span>
+    </div>
+        <a href="https://www.facebook.com/profile.php?id=100064037290647" target="_blank" class="me-4 text-reset">
+          <i class="bi bi-facebook"></i>
+        </a>
+    </div>
+  </section>
+    <div class="container text-center text-md-start mt-0 bg-dark">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Conocenos
+          </h6>
+          <p align="left">
+          Motoparte El Triciclo, nos esforzamos por brindarte una experiencia de compra excepcional. Nuestro personal amable y conocedor está listo para guiarte y brindarte asesoramiento experto en cada paso del camino. Queremos que te sientas como en casa mientras navegas por nuestro amplio inventario y descubres todo lo que necesitas para personalizar y mejorar tu moto.
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Cuando nos eligen
+          </h6>
+          <p align="left"> Cuando eliges Motoparte El Triciclo, eliges confianza y profesionalismo. Nuestros dedicados mecánicos están siempre listos para ayudarte, asesorarte y resolver cualquier problema que tengas con tu moto. No importa si es una reparación pequeña o una mejora importante, estamos aquí para asegurarnos de que tu moto esté en perfecto estado y lista para conquistar la carretera. </p>
+        </div>
+      </div>
+    </div>
+    
+  <div class="text-center bd-dark">
+    © 2023Copyright: Ing. Software 2 año 2023
+  </div>
+  </section>
+</footer>
+  </body>
